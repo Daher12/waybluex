eb9da94c1795d7729301c71b711c3dbd221293bd
Daher12 <133640261+Daher12@users.noreply.github.com>
2025-09-01 12:34:24 +0200

    Update removeunusedrepos.sh

--- files/scripts/removeunusedrepos.sh
+++ files/scripts/removeunusedrepos.sh
@@ -8,3 +8,8 @@ rm -f /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 rm -f /etc/yum.repos.d/eyecantcu-supergfxctl.repo
 rm -f /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 rm -f /etc/yum.repos.d/nvidia-container-toolkit.repo
+rm -f /etc/yum.repos.d/tailscale.repo 
+rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:chenxiaolong:sbctl.repo 
+rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:monkeygold:nautilus-open-any-terminal.repo 
+rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:solopasha:hyprland.repo 
+rm -f /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:tofik:nwg-shell.repo 
